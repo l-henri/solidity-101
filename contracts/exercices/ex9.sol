8ee8bc92372116797e09c7126f3be57536aa673f
Henri <Henri.lieutaud@gmail.com>
2019-02-21 17:46:48 +0100

    Obfuscated exercice 9

--- contracts/exercices/ex9.sol
+++ contracts/exercices/ex9.sol
@@ -1,54 +1,26 @@
-
 pragma solidity ^0.4.24;
 
 
 import "../exerciceTemplate.sol";
 
 /*
-Exercice 9: Visualizing code online
+Exercice 9: Visualizing code through Github history of commits
 In this exercice, you need to:
-- Use Etherscan to visualize this contract's code
+- Use Github to explore this contract's code history of revision
+- Find the last visible version of the contract code
 - Use a function 
 - Your points are credited by the contract
-*/
+*/ 
 
 /*
 What you need to know to complete this exercice
 A) What was included in the previous exercices
-B) Etherscan.io https://etherscan.io/ lets you visualize the source code of contracts whose creator disclosed it. 
+B) The history of contract modification is visible on https://github.com/l-henri/solidity-101
+C) Only using the ABI of this contract puts you at risk of not getting the points you deserve
 
 */
-contract ex9 is exerciceTemplate {
+contract ex9_instructions is exerciceTemplate {
 
-  event aLazyStudent(address _lazyStudent);
+// Find the code somewhere else
 
-	constructor(address _studentsOrganAddres, address _teachersOrganAddress, address _pointsManagerContractAddress) 
-  exerciceTemplate(_studentsOrganAddres, _teachersOrganAddress, _pointsManagerContractAddress) 
-  public
-  {}
-
-
-  function collectYourPoints(uint _aValueToInput) 
-  public 
-  canWorkOnExercice 
-  {
-    require(_aValueToInput == 982738);
-    emit aLazyStudent(msg.sender);
-
-    // Validating exercice WITHOUT crediting points. Be careful, calling this function will NOT credit points to you!
-    validateExercice(msg.sender);
-
-  }
-
-  function collectYourPointsAgain(uint _aValueToInput) 
-  public 
-  canWorkOnExercice 
-  {
-    require(_aValueToInput == 972738);
-
-    // Validating exercice, getting points
-    validateExercice(msg.sender);
-    creditStudent(200, msg.sender);
-
-  }
 }
