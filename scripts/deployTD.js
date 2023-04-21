333589b89f9061336bc5e4355ab1af118e47946f
l-henri <henri.lieutaud@gmail.com>
2023-04-21 14:04:45 +0200

    Adding more logs

--- scripts/deployTD.js
+++ scripts/deployTD.js
@@ -45,6 +45,22 @@ async function main() {
     const Ex12Contract = await Ex12.deploy(erc20.address);
     const Ex14Contract = await Ex14.deploy(erc20.address);
     const Ex15Contract = await Ex15.deploy(erc20.address);
+    console.log("    \""+erc20.address+"\",  // (erc20)")
+    console.log("    \""+Ex1Contract.address+"\",  // (Ex1Contract)")
+    console.log("    \""+Ex2Contract.address+"\",  // (Ex2Contract)")
+    console.log("    \""+Ex3Contract.address+"\",  // (Ex3Contract)")
+    console.log("    \""+Ex4Contract.address+"\",  // (Ex4Contract)")
+    console.log("    \""+Ex5Contract.address+"\",  // (Ex5Contract)")
+    console.log("    \""+Ex6Contract.address+"\",  // (Ex6Contract)")
+    console.log("    \""+Ex7Contract.address+"\",  // (Ex7Contract)")
+    console.log("    \""+Ex8Contract.address+"\",  // (Ex8Contract)")
+    console.log("    \""+Ex9Contract.address+"\",  // (Ex9Contract)")
+    console.log("    \""+Ex10Contract.address+"\",  // (Ex10Contract)")
+    console.log("    \""+Ex11Contract.address+"\",  // (Ex11Contract)")
+    console.log("    \""+Ex11bContract.address+"\",  // (Ex11bContract)")
+    console.log("    \""+Ex12Contract.address+"\",  // (Ex12Contract)")
+    console.log("    \""+Ex14Contract.address+"\",  // (Ex14Contract)")
+    console.log("    \""+Ex15Contract.address+"\",  // (Ex15Contract)")
   
     console.log("Setting random value store")
   
